da6cb333729bb852adaf26ff30bc27ef07e3c9c0
Lorenzo Notaro <lorenzo.notaro1@gmail.com>
2026-08-13 21:39:13 +0200

    pdate make.sh to include source files and adjust memory data definitions in ddi.lnasm and mem.lnasm

--- programs/ln-bios/make.sh
+++ programs/ln-bios/make.sh
@@ -1,3 +1,3 @@
-SRC_FILES=$(find src/lnasm/*/*.lnasm src/lnasm/*.lnasm src/lnc/*.lnc src/lnc/*/*.lnc -type f -nowarn | xargs echo)
+SRC_FILES="ln-bios.lnasm ln-bios.lnc"
 echo Source files: $SRC_FILES
 lnc $SRC_FILES -lf linker.cfg -oD="ROM,RAM,D1" -oA bios.lnasm -oB bios.out -oS bios.sym -oI bios.immediate.txt -oM bios.intermediate.txt -oA="__lncout.lnasm" -I="include/"
