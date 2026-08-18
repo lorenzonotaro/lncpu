6df662305bd8f8db1d36b963c3839d3ed095f3e9
Lorenzo Notaro <lorenzo.notaro1@gmail.com>
2026-08-18 10:56:00 +0200

    progress on ln-bios/test-os

--- programs/ln-bios/make.sh
+++ programs/ln-bios/make.sh
@@ -1,3 +1,8 @@
 SRC_FILES="ln-bios.lnasm ln-bios.lnc"
 echo Source files: $SRC_FILES
-lnc $SRC_FILES -lf linker.cfg -oD="ROM,RAM,D1" -oA bios.lnasm -oB bios.out -oS bios.sym -oI bios.immediate.txt -oM bios.intermediate.txt -oA="__lncout.lnasm" -I="include/"
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+lnc $SRC_FILES -lf linker.cfg -oD="ROM,RAM,D1" -oA build/bios.lnasm -oB build/bios.out -oS build/bios.sym -oI build/bios.immediate.txt -oM build/bios.intermediate.txt -oA="build/__lncout.lnasm" -I="include/"
--- programs/test-os/make.sh
+++ programs/test-os/make.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
 echo "Building ln-bios..."
 cd "$SCRIPT_DIR/../ln-bios"
 ./make.sh || {
@@ -13,9 +17,6 @@ cd "$SCRIPT_DIR/../ln-bios"
 echo "Building test-os..."
 cd "$SCRIPT_DIR/../test-os"
 
-echo "Cleaning up..."
-rm -f test-os.out.imm.txt test-os.out.ir.txt test-os.out.lnasm test-os.out
-
 SOURCES=()
 while IFS= read -r -d '' f; do
     SOURCES+=("$f")
@@ -24,12 +25,12 @@ done < <(find src -type f \( -name '*.lnc' -o -name '*.lnasm' \) -print0)
 echo "Source files: ${SOURCES[*]}"
 
 lnc "${SOURCES[@]}" \
-    -oD="D0" \
-    -oI="test-os.out.imm.txt" \
-    -oM="test-os.out.ir.txt" \
-    -oA="__lncout.lnasm" \
-    -oB="test-os.out" \
-    -S="../ln-bios/bios.sym" \
+    -oD="ROM" \
+    -oI="build/test-os.out.imm.txt" \
+    -oM="build/test-os.out.ir.txt" \
+    -oA="build/__lncout.lnasm" \
+    -oB="build/test-os.out" \
+    -S="../ln-bios/build/bios.sym" \
     -I="../ln-bios/include" || {
         echo "Build failed!"
         exit 1
@@ -37,5 +38,5 @@ lnc "${SOURCES[@]}" \
 
 if [[ "${1:-}" == "--run" ]]; then
     echo "Running project..."
-    lncpu_emu --rom="../ln-bios/bios.out" -t D1 --d0=test-os_D0.out ${2:-}
+    lncpu_emu --rom="../ln-bios/build/bios.out" -t D1 --d0=build/test-os.out ${2:-}
 fi
